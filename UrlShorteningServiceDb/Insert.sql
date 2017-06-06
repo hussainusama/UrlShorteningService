6057e27d8c1874047cc378f042db7fe0cfc96346
Usama Hussain <hussain.usama@gmail.com>
2017-06-07 03:02:35 +0800

    Updated database

--- UrlShorteningServiceDb/Insert.sql
+++ UrlShorteningServiceDb/Insert.sql
@@ -4,31 +4,7 @@
 AS
 	SET NOCOUNT ON;
 	
-	BEGIN TRY
-
-     INSERT INTO UrlMapping VALUES (@Url);
+	 INSERT INTO UrlMapping VALUES (@Url);
 	 SET @Id = SCOPE_IDENTITY()
-
-	END TRY
-	BEGIN CATCH
-
-	DECLARE @ErrorMessage NVARCHAR(4000);  
-    DECLARE @ErrorSeverity INT;  
-    DECLARE @ErrorState INT;  
-
-  
-	 IF ERROR_NUMBER() <> 2627
-	 BEGIN
-	    SELECT
-            @ErrorMessage = ERROR_MESSAGE(), 
-            @ErrorSeverity = ERROR_SEVERITY(), 
-            @ErrorState = ERROR_STATE(); 
-
-		RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState); 
- 	 END
-	 ELSE
-      SELECT @Id = Id FROM UrlMapping WHERE [Url] = @Url
-
-	END CATCH
 
 RETURN 0
