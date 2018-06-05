ca6f77eae6965ef97390b8541459a2d357113a54
Usama Hussein <ush@sitecore.net>
2018-06-05 11:26:27 +0800

    Setup AutoFixture

---
+++ UrlShorteningService.Tests/Infrastructure/DefaultCustomization.cs
@@ -0,0 +1,13 @@
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+
+namespace UrlShorteningService.Tests.Infrastructure
+{
+    internal class DefaultCustomization : CompositeCustomization
+    {
+        public DefaultCustomization() : base(
+            new AutoNSubstituteCustomization { ConfigureMembers = true})
+        {
+        }
+    }
+}
