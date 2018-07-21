30721e88f6a5515df9a0bb57b9cf183dbefb2df6
Usama Hussain <hussain.usama@gmail.com>
2018-07-21 22:15:02 +0800

    Remove unused folders

--- UrlShorteningService/UrlProcessors/IBase62Encoder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UrlShorteningService.UrlProcessors
-{
-    public interface IBase62Encoder
-    {
-        string Encode(int val);
-
-        int Decode(string val);
-    }
-}
--- UrlShorteningService/UrlProcessors/IUrlRetriever.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace UrlShorteningService.UrlProcessors
-{
-    internal interface IUrlRetriever
-    {
-        Task<string> RetreiveUrlAsync(string shortUrl);
-    }
-}
